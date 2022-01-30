f57205e1319509255bbac89fccd5c8df8d99a6c3
alejandrodavidmalavet <61026152+alejandrodavidmalavet@users.noreply.github.com>
2022-01-30 13:23:18 -0600

    Two Characters

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    private Vector2 movementInput;
+
+    public void Update()
+    {
+        transform.Translate(new Vector2(movementInput.x, movementInput.y) * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+}
