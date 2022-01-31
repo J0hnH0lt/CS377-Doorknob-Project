ff60854a60c151fd5d89aa99586993c86cc99bd7
alejandrodavidmalavet <61026152+alejandrodavidmalavet@users.noreply.github.com>
2022-01-31 13:35:14 -0600

    Prefab Stuff

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
-{
-    public float speed = 5;
+{   
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
     private Vector2 movementInput;
 
     public void Update()
     {
-        transform.Translate(new Vector2(movementInput.x, movementInput.y) * speed * Time.deltaTime);
+        Vector2 movementDirection = new Vector2(movementInput.x, movementInput.y);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
