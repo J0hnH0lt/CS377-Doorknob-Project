154265440c913f3518f439e9bd70294320adef5f
Philip Clement <pclement803@gmail.com>
2022-02-06 20:10:15 -0600

    Main Menu Created

---
+++ Assets/MainMenuInitialization.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+public class MainMenuInitialization : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Main Menu Init");
+        Screen.SetResolution(1280, 720, true);
+    }
+}
